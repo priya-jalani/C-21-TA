850d6d5bb6728f126b6bda326fd02f814fc1d05f
priya-jalani <72224049+priya-jalani@users.noreply.github.com>
2021-01-03 19:55:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,49 +1,65 @@
-var fixedRect, movingRect;
-var gameObject1, gameObject2, gameObject3, gameObject4;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
 
-function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(600, 400, 50, 80);
-  fixedRect.shapeColor = "green";
- 
-  movingRect = createSprite(800, 400,80,30);
-  movingRect.shapeColor = "green";
+var engine, world;
+var box1, pig1;
+var backgroundImg;
+var platform;
 
-  gameObject1 = createSprite(100, 100, 50, 50);
-  gameObject1.shapeColor = "green";
-  gameObject2 = createSprite(200, 100, 50, 50);
-  gameObject2.shapeColor = "green";
-  gameObject3 = createSprite(300, 100, 50, 50);
-  gameObject3.shapeColor = "green";
-  gameObject4 = createSprite(400, 100, 50, 50);
-  gameObject4.shapeColor = "green";
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
 }
 
-function draw() {
-  background(0,0,0);  
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
 
-  if(isTouching(movingRect, gameObject1)){
-    movingRect.shapeColor = "blue";
-    gameObject1.shapeColor = "blue";
-  }
-  else {
-    movingRect.shapeColor = "green";
-    gameObject1.shapeColor = "green";
-  }
-  drawSprites();
-}
-
-function isTouching(object1,object2){
-  if (object1.x - object2.x < object2.width/2 + object1.width/2
-    && object2.x - object1.x < object2.width/2 + object1.width/2
-    && object1.y - object2.y < object2.height/2 + object1.height/2
-    && object2.y - object2.y < object2.height/2 + object1.height/2) {
     
-    return true;
-  }
-  else {
-    return false;
-  } 
+    ground = new Ground(600,height,1200,20)
+    platform = new Ground(150, 305, 300, 170)
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(100,100);
+
+}
+
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
 }
